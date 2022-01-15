f535e31623445d264d1d4c9fe79544f836619337
Ayoub Boulbaz <aboulbaz@e2r7p12.1337.ma>
2022-01-15 16:39:14 +0100

    integrated the parsing and the server

--- request_response/request_handler.hpp
+++ request_response/request_handler.hpp
@@ -36,22 +36,29 @@ public:
 		size_t index;
 
 		server_name = req_map["Host"];
+		
 		if ((found = server_name.find(":"))!= std::string::npos)
 				server_name = server_name.substr(0,found);	
 		for (size_t i = 0; i < Servs.size(); i++)
 		{
 			server_names = Servs[i].get_server_name();
-			if (server_names[server_name]) {
+			if (server_names.find(server_name) != server_names.end()) {
 				std::vector<Location> locations = Servs[i].get_location();
 				for (size_t j = 0; j < locations.size(); j++)
 				{
-					if (req_map["URL"] == locations[j].get_path())
-						return Response(Servs[i].get_root() + locations[j].get_path() +"/index.html");
+					std::vector<std::string> allowed_methods = locations[j].get_request_method();
+					if (req_map["URL"] == locations[j].get_path()) // Allowed Method
+					{
+						if ((allowed_methods.size() == 0 || std::find(allowed_methods.begin(), allowed_methods.end(), req_map["Method"]) != allowed_methods.end()))
+							return Response(Servs[i].get_root() + locations[j].get_path() +"/index.html"); // indexes Autoindexn
+						std::cout << Servs[i].get_error_page()["500"] << std::endl;
+						return Response(500, Servs[Servs.size() - 1].get_root() + Servs[i].get_error_page()["500"]);
+					}	
 				}
 			}
 		}
 		std::map<std::string, std::string> errorPages = Servs[Servs.size() - 1].get_error_page();
-		return Response(502, Servs[Servs.size() - 1].get_root() + errorPages["502"]);
+		return Response(404, Servs[Servs.size() - 1].get_root() + errorPages["404"]);
 	}
 
 	Response Bootstrap() {	
