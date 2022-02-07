89f1c2c599f2192263628b85ff75d02d7c5f8849
Ayoub Boulbaz <aboulbaz@e2r7p12.1337.ma>
2022-02-07 19:34:03 +0100

    POST Request init

--- request_response/request_handler.hpp
+++ request_response/request_handler.hpp
@@ -86,17 +86,18 @@ public:
 				if (body[i].filename != "")
 				{
 					std::cout << RED << " +++ IT IS A FILE +++" << RESET << std::endl;
-					// std::cout << server.get_root() + location.get_path() + body[i].filename << std::endl;
-					std::cout << body[i].body << std::endl;
-					if (body[i].body.length() > server.get_client_max_body_size())
+					std::cout << "Client size Body => " << body[i].body.length() << std::endl;
+					std::cout << "Max Client => " <<server.get_client_max_body_size() << std::endl;
+					if (body[i].body.length() > 10)
 						return Response(server, server.get_root() + server.get_error_page()["413"], "POST", "413");
 					if(stat((server.get_root() + location.get_path()).c_str(), &fileStat) < 0)    
         				return Response(server, server.get_root() + server.get_error_page()["502"], "POST", "502");
 					if(!(fileStat.st_mode & S_IWUSR))    
         				return Response(server, server.get_root() + server.get_error_page()["403"], "POST", "403");
-					int fd = open((server.get_root() + location.get_path() + body[i].filename).c_str(), O_CREAT | O_RDWR, 0644);
+					int fd = open((server.get_root() + "/" + location.get_path() + "/" + body[i].filename).c_str(), O_CREAT | O_RDWR, 0644);
+					std::cout << "Get the file's path => " << (server.get_root() + "/" + body[i].filename) << std::endl;
 					write(fd, body[i].body.c_str(), body[i].body.length());
-					// std::cout << RED << " ++++++++++++++++++++" << RESET << std::endl;
+					std::cout << RED << " ++++++++++++++++++++" << RESET << std::endl;
 				}
 				else
 				{
