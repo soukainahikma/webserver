fb73942ea0c26a99bdb25333aaa71a246aa49b10
soukainahikma <soukainahikma@gmail.com>
2022-02-14 13:42:48 +0100

    fixing request comming from the post request

--- server/helper.cpp
+++ server/helper.cpp
@@ -4,6 +4,7 @@
 #include "../request_response/request_handler.hpp"
 #include <sys/ioctl.h>
 
+#define SIZE 65507
 int fileCheck(std::string fileName, std::string req_type)
 {
 	if (!access(fileName.c_str(), F_OK))
@@ -19,20 +20,35 @@ int fileCheck(std::string fileName, std::string req_type)
 void connection_handler(int i,RequestHandler &req_handler, int port)
 {
 	int size;
+	std::string files;
+	std::ofstream file("req.txt");
+	int		ret;
 
-	if (ioctl(i, FIONREAD, &size) == -1)
-		throw std::runtime_error("INTERNAL SERVER ERROR");
-	char *buffer = (char *)malloc(size * sizeof(char));
-	int read_val = read(i, buffer, size);
-	if (buffer[0] != 0)
+	// if (ioctl(i, FIONREAD, &size) == -1)
+	// 	throw std::runtime_error("INTERNAL SERVER ERROR");
+	char *buffer =(char*) malloc(sizeof(char) * 100);
+	bzero(buffer,100);
+	while((ret = recv(i, buffer, 99, 0))>0)
 	{
-		Request req(buffer, port);
-		req_handler.setRequest(req);
-		Response resp = req_handler.Bootstrap();
-		const char *hello = resp.get_header().c_str();
-		// std::cout << hello << std::endl;
-		send(i, hello, strlen(hello), 0);
-		close(i);
-	}
+		// std::cout<< buffer<<std::endl;
+		std::cout<< "ret : " << ret << std::endl;
+		files += std::string(buffer);
+		bzero(buffer,100);
+		if(ret< 99)
+			break;
+	};
+	std::cout<< "last ret : " << ret << std::endl;
+	file << files << std::endl;
+	file.close();
+	// if (buffer[0] != 0)
+	// {
+	// 	Request req(buffer, port);
+	// 	req_handler.setRequest(req);
+	// 	Response resp = req_handler.Bootstrap();
+	// 	const char *hello = resp.get_header().c_str();
+	// 	// std::cout << hello << std::endl;
+	// 	send(i, hello, strlen(hello), 0);
+	// 	close(i);
+	// }
 	free(buffer);
 }
