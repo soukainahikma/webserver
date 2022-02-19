2d83432a4ad448bd32ae43c3ff02229423c6943e
Choaib Abouelwafa <cabouelw@e2r8p13.1337.ma>
2022-02-19 21:51:30 +0100

    fix cgi post

--- parsing/cgi.cpp
+++ parsing/cgi.cpp
@@ -15,8 +15,12 @@ char	**init_env(std::string status, std::string path, std::string page, Request 
 	setenv("SCRIPT_NAME", page.c_str(), 1);
 	setenv("SCRIPT_FILENAME", page.c_str(), 1);
 	setenv("CONTENT_LENGTH", req.getRequest()["Content-Length"].c_str(), 1);
+	// setenv("name=chb&tele=dsfsdf", "19", 1);
 	setenv("REQUEST_METHOD", req.getRequest()["Method"].c_str(), 1);
-	setenv("CONTENT_TYPE", req.getRequest()["Content-Type"].c_str(), 1);
+	if (req.getRequest()["Content-Type"].find("multipart/form-data") != std::string::npos)
+		setenv("CONTENT_TYPE", "application/x-www-form-urlencoded", 1);
+	else
+		setenv("CONTENT_TYPE", req.getRequest()["Content-Type"].c_str(), 1);
 	setenv("PATH_INFO", path.c_str(), 1);
 	setenv("PATH_TRANSLATED", path.c_str(), 1);
 	setenv("QUERY_STRING", req.getBodyString().c_str(), 1);
@@ -44,6 +48,7 @@ std::string get_root(std::string root)
 std::string runCgi(std::string root, std::string path, std::string page, std::string &status, Request &req)
 {
 	int pipefd[2];
+	int pipefd_data[2];
 	char **args;
 	std::string body = "";
 	int size_read = 1024;
@@ -52,6 +57,21 @@ std::string runCgi(std::string root, std::string path, std::string page, std::st
 	if (size_read < 6)
 		return NULL;
 	char **env = init_env(status, path, page, req);
+	pipe(pipefd_data);
+	body = req.getBodyString().c_str();
+	if (req.getRequest()["Content-Type"].find("multipart/form-data") != std::string::npos)
+	{
+		body = "";
+		for (size_t i = 0; i < req.getBodyStructs().size(); i++)
+		{
+			if (i != 0)
+				body += "&";
+			body += req.getBodyStructs()[i].name + "=" + req.getBodyStructs()[i].body;
+		}
+	}
+	
+	write(pipefd_data[1], body.c_str(), req.getBodyString().size());
+	// std::cerr << "++++++++++++++|" << req.getBodyString() << "|+++++++++hna\n";
 	if (page.find(".py") != std::string::npos)
 	{
 		args = new char*[3];
@@ -62,6 +82,7 @@ std::string runCgi(std::string root, std::string path, std::string page, std::st
 	else
 	{
 		args = new char*[2];
+		// write(pipefd_data[1], req.getBodyString().c_str(), req.getBodyString().size());
 		root_c = get_root(page) + "/parsing/php-cgi";
 		args[0] = (char*)(root_c.c_str());
 		args[1] = NULL;
@@ -71,6 +92,9 @@ std::string runCgi(std::string root, std::string path, std::string page, std::st
 	if (!pid)
 	{
 		dup2(pipefd[1], 1);
+		close(pipefd_data[1]);
+		dup2(pipefd_data[0], 0);
+		close(pipefd_data[0]);
 		close(pipefd[0]);
 		close(pipefd[1]);
 		execve(args[0], args, (char **)env);
@@ -78,6 +102,8 @@ std::string runCgi(std::string root, std::string path, std::string page, std::st
 	}
 	else
 	{
+		close(pipefd_data[0]);
+		close(pipefd_data[1]);
 		wait(&pid);
 		int r;
 		bzero(buffer, size_read);
